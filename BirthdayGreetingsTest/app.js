03f1e11390102ac460d4c5d96c2844e8241811bc
MaksymilianSze <Maksymilian.Szejko@city.ac.uk>
2023-01-11 13:08:18 +0000

    Fix bug, create function convertDate and update comments and paths

--- BirthdayGreetingsTest/app.js
+++ BirthdayGreetingsTest/app.js
@@ -2,20 +2,22 @@ import { sendGreeting } from "./sendGreeting.js";
 import { retrieveBirthdaysFromCSV } from "./retrieveBirthdaysFromCSV.js";
 import { retrieveBirthdaysFromDB } from "./retrieveBirthdaysFromDB.js";
 
-console.log(retrieveBirthdaysFromCSV("2000/02/28"));
-console.log("\n");
-console.log(await retrieveBirthdaysFromDB("2000/02/28"));
+sendGreeting(retrieveBirthdaysFromCSV("2000-02-28"), "email"); // Should find 2 friends and send greetings to both via email
 
-sendGreeting(retrieveBirthdaysFromCSV("2000/02/28"), "email");
+sendGreeting(retrieveBirthdaysFromCSV("2000-02-28"), "sms"); // Should find 2 friends and send greetings to both via sms
 
-sendGreeting(retrieveBirthdaysFromCSV("2000/02/28"), "sms");
+sendGreeting(retrieveBirthdaysFromCSV("1997-02-12"), "email"); // Should find no friends and throw an error
 
-sendGreeting(retrieveBirthdaysFromCSV("1997/02/12"), "email");
+sendGreeting(retrieveBirthdaysFromCSV("1997-10-08"), "email"); // Should find 1 friend and send a greeting to them via email
 
-sendGreeting(retrieveBirthdaysFromCSV("1997/10/08"), "email");
+sendGreeting(await retrieveBirthdaysFromDB("1999-02-12"), "email"); // Should find no friends and throw an error
 
-sendGreeting(retrieveBirthdaysFromDB("1997/02/12"), "email");
+sendGreeting(await retrieveBirthdaysFromDB("1999-09-11"), "email"); // Should find 1 friend and send a greeting to them via email
 
-sendGreeting(retrieveBirthdaysFromCSV("1902/12"), "email");
+sendGreeting(retrieveBirthdaysFromCSV("1902-12"), "email"); // Should throw an error because date is in the wrong format
 
-sendGreeting(retrieveBirthdaysFromCSV("1997/"), "email");
+sendGreeting(retrieveBirthdaysFromCSV("seg45747"), "email"); // Should throw an error because this isn't a date
+
+sendGreeting(await retrieveBirthdaysFromDB("2022-02-29"), "email"); // Should throw an error because 2022 wasn't a leap year
+
+sendGreeting(await retrieveBirthdaysFromDB("2020-02-28"), "email"); // Should find 2 friends and send greetings to both via email
